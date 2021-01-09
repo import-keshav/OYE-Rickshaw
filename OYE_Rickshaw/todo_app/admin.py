e06f0d7e045c487fbb9928e0e1fefbaf33ae9189
Keshav Bathla <keshavbathla2017@gmail.com>
2021-01-09 06:05:29 +0530

    Added Admin File

--- OYE_Rickshaw/todo_app/admin.py
+++ OYE_Rickshaw/todo_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.TODO)
+class TODOAdmin(admin.ModelAdmin):
+	list_display = ('title', 'description', 'state', 'priority', 'date', 'id')
+	search_fields = ('title', 'state', 'priority', 'date')
+	list_filter = ('state', 'priority')
