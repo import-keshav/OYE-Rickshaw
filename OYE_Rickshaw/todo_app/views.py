1813a562c465ebdea5295bc718442e5b8fd614f8
Keshav Bathla <keshavbathla2017@gmail.com>
2021-01-09 06:05:47 +0530

    Added CRUD operations API

--- OYE_Rickshaw/todo_app/views.py
+++ OYE_Rickshaw/todo_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, status, filters
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+class GetTODO(generics.ListAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = serializers.TODOSerializer
+    queryset = models.TODO.objects.all()
+
+
+class CreateTODO(generics.CreateAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = serializers.TODOSerializer
+
+
+class UpdateTODO(generics.UpdateAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = serializers.TODOSerializer
+    queryset = models.TODO.objects.all()
+
+
+class DeleteTODO(generics.DestroyAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = serializers.TODOSerializer
+    queryset = models.TODO.objects.all()
+
+
+class SearchTODO(generics.ListAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = serializers.TODOSerializer
+    queryset = models.TODO.objects.all()
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['state', 'date', 'title', 'priority']
+    search_fields = ['state', 'date', 'title', 'priority']
